ded94aa6c5ab4076b9a82b171d0b6a4504b6580c
Arda Yalçın <yalcinarda734@gmail.com>
2026-04-28 13:53:28 +0300

    Logout eklendi

--- IdEntity/Controllers/AccountController.cs
+++ IdEntity/Controllers/AccountController.cs
@@ -87,6 +87,11 @@ namespace IdEntity.Controllers
             }
             return View();
         }
-
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Home");
+        }
     }
 }
